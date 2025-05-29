e4f602725d3fd6c29092876db3f80af0ef0b69f1
indranilsharma <indranilsharma123@gmail.com>
2025-05-29 22:45:39 +0530

    Update TransactionRepository.java

--- src/main/java/com/emc/jcore/reward_system/repository/TransactionRepository.java
+++ src/main/java/com/emc/jcore/reward_system/repository/TransactionRepository.java
@@ -4,7 +4,9 @@ import com.emc.jcore.reward_system.entity.RewordTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+/**
+ *JPA reposistory added
+ */
 public interface TransactionRepository extends JpaRepository<RewordTransaction, Long> {
     List<RewordTransaction> findByCustomerId(String customerId);
 }
