e872d0a939fd894fa179739bdf6232b3dfe8550b
indranilsharma <indranilsharma123@gmail.com>
2025-05-29 22:38:55 +0530

    post mapping updated.

--- src/main/java/com/emc/jcore/reward_system/controller/RewardController.java
+++ src/main/java/com/emc/jcore/reward_system/controller/RewardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
- *
+ *Controller method for post mapping which is returning list
  */
 @RestController
 @RequestMapping(value = "/api/rewards")
